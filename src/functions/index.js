c3ca62ceff2921f4b2319466c036941e61eb7934
白彩恋 <shiro@oom-wg.dev>
2026-02-02 19:28:15 +0800

    Update index.js

--- src/functions/index.js
+++ src/functions/index.js
@@ -3,5 +3,5 @@ export const onRequest = ctx =>
     (url.hostname = url.hostname.startsWith('www.')
       ? url.hostname.slice(4)
       : 'www.' + url.hostname),
-    Response.redirect(url, 301)
+    Response.redirect(`${url}`, 301)
   ))(new URL(ctx.request.url))
